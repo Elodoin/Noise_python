a75c1446eae0f01e446df05a10fed7431f8da639
chengxin jiang <chengxinjiang@gmail.com>
2019-07-11 14:15:35 +1000

    updates of NoisePy

---
+++ src/S0_dowload_ASDF_MPI.py
@@ -0,0 +1,216 @@
+import sys
+import time
+import obspy
+import pyasdf
+import os, glob
+import numpy as np
+import pandas as pd
+import noise_module
+from mpi4py import MPI
+from obspy import UTCDateTime
+from obspy.clients.fdsn import Client
+
+if not sys.warnoptions:
+    import warnings
+    warnings.simplefilter("ignore")
+
+'''
+This downloading script:
+    1) downloads data chunck on your choice of length and Client or pre-compiled station list;
+    2) cleans up the traces including gaps, removing instrumental response, downsampling and trim;
+    3) saves data into ASDF format;
+    4) use MPI to speep up downloading process.
+
+Authors: Marine Denolle (mdenolle@fas.harvard.edu) - 11/16/18,06/08/19
+         Chengxin Jiang (chengxin_jiang@fas.harvard.edu) - 02/22/19,07/01/19
+         
+Note: 1. segmentation fault while manipulating obspy stream can come from too large data in memory:
+     reduce the inc_hours variable
+      2. if choose to download stations from an existing CSV files, station with the same name but 
+     different channel is regarded as different stations
+
+A beginning of NoisePy journey! 
+'''
+
+#######################################################
+################PARAMETER SECTION######################
+#######################################################
+tt0=time.time()
+
+# paths and filenames
+rootpath = '/mnt/data0/NZ/XCORR' 
+direc  = os.path.join(rootpath,'RAW_DATA')      # where to store the downloaded data
+dlist  = os.path.join(direc,'station.lst')      # CSV file for station location info
+
+# download parameters
+client    = Client('GEONET')                    # client/data center. see https://docs.obspy.org/packages/obspy.clients.fdsn.html for a list
+down_list = False                               # download stations from pre-compiled list
+oput_CSV  = True                                # output station.list to a CSV file to be used in later stacking steps
+flag      = True                                # print progress when running the script
+NewFreq   = 10                                  # resampling at X samples per seconds 
+rm_resp   = False                               # False to not remove, True to remove, but 'inv' to remove with inventory
+respdir   = 'none'                              # output response directory (required if rm_resp is true and other than inv)
+freqmin   = 0.05                                # pre filtering frequency bandwidth
+freqmax   = 4
+
+# station information 
+lamin,lomin,lamax,lomax=-46.5,168,-38,175       # regional box: min lat, min lon, max lat, max lon
+dchan= ['HH*']                                  # channel if down_list=false
+dnet = ["NZ"]                                   # network  
+dsta = ["M?Z"]                                  # station (do either one station or *)
+start_date = ["2018_05_01_0_0_0"]               # start date of download
+end_date   = ["2018_05_04_0_0_0"]               # end date of download
+inc_hours  = 48                                 # length of data for each request (in hour)
+
+# time tags
+starttime = obspy.UTCDateTime(start_date[0])       
+endtime   = obspy.UTCDateTime(end_date[0])
+if flag:
+    print('station.list selected [%s] for data from %s to %s with %sh interval'%(down_list,starttime,endtime,inc_hours))
+
+# assemble parameters for pre-processing
+prepro_para = {'rm_resp':rm_resp,'respdir':respdir,'freqmin':freqmin,'freqmax':freqmax,\
+    'samp_freq':NewFreq,'start_date':start_date,'end_date':end_date,'inc_hours':inc_hours}
+metadata = os.path.join(direc,'download_info.txt') 
+
+# prepare station info (existing station list vs. fetching from client)
+if down_list:
+    if not os.path.isfile(dlist):
+        raise IOError('file %s not exist! double check!' % dlist)
+
+    # read station info from list
+    locs = pd.read_csv(dlist)                   
+    nsta = len(locs)
+    chan = list(locs.iloc[:]['channel'])
+    net  = list(locs.iloc[:]['network'])
+    sta  = list(locs.iloc[:]['station'])
+    lat  = list(locs.iloc[:]['latitude'])
+    lon  = list(locs.iloc[:]['longitude'])
+
+    # location info: useful for some occasion
+    try:
+        location = list(locs.iloc[:]['location'])
+    except Exception as e:
+        location = ['*']*nsta
+
+else:
+
+    # gather station info
+    try:
+        inv = client.get_stations(network=dnet[0],station=dsta[0],channel=dchan[0],location='*', \
+            starttime=starttime,endtime=endtime,minlatitude=lamin,maxlatitude=lamax, \
+            minlongitude=lomin, maxlongitude=lomax,level="response")
+        # make a selection to remove redundent channel (it indeed happens!)
+        inv1 = inv.select(network=dnet[0],station=dsta[0],channel=dchan[0],starttime=starttime,\
+            endtime=endtime,location='*') 
+        if flag:print(inv1)
+    except Exception as e:
+        print('Abort! '+str(e))
+        exit()
+
+    # calculate the total number of channels to download
+    sta=[];net=[];chan=[];location=[]
+    nsta=0
+    for K in inv:
+        for sta1 in K:
+            for chan1 in sta1:
+                sta.append(sta1.code)
+                net.append(K.code)
+                chan.append(chan1.code)
+                location.append(chan1.location_code)
+                nsta+=1
+    prepro_para['nsta'] = nsta
+
+
+########################################################
+#################DOWNLOAD SECTION#######################
+########################################################
+
+#--------MPI---------
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+if rank==0:
+    if not os.path.isdir(direc):os.mkdir(direc)
+    # output station list
+    if not down_list:     
+        if oput_CSV:noise_module.make_stationlist_CSV(inv,direc)
+    # save parameters for future reference
+    fout = open(metadata,'w')
+    fout.write(str(prepro_para));fout.close()
+
+    # get MPI variables ready 
+    all_chunck = noise_module.get_event_list(start_date[0],end_date[0],inc_hours)
+    if len(all_chunck)<1:
+        raise ValueError('Abort! no data chunck between %s and %s' % (start_date[0],end_date[0]))
+    splits = len(all_chunck)-1
+else:
+    splits,all_chunck = [None for _ in range(2)]
+
+# broadcast the variables
+splits = comm.bcast(splits,root=0)
+all_chunck  = comm.bcast(all_chunck,root=0)
+extra = splits % size
+
+#--------MPI: loop through each time chunck--------
+for ick in range (rank,splits+size-extra,size):
+    if ick<splits:
+
+        s1=obspy.UTCDateTime(all_chunck[ick])
+        s2=obspy.UTCDateTime(all_chunck[ick+1]) 
+        date_info = {'starttime':s1,'endtime':s2} 
+        
+        # filename of the ASDF file
+        ff=os.path.join(direc,all_chunck[ick]+'T'+all_chunck[ick+1]+'.h5')
+        if not os.path.isfile(ff):
+            with pyasdf.ASDFDataSet(ff,mpi=False,compression="gzip-3",mode='w') as ds:
+
+                # loop through each channel
+                for ista in range(nsta):
+
+                    # select from existing inventory database
+                    if down_list:
+                        try:
+                            sta_inv = client.get_stations(network=net[ista],station=sta[ista],\
+                                location=location[ista],starttime=s1,endtime=s2,level="response")
+                        except Exception as e:
+                            print(e);continue
+                        if sta_inv[0][0][0].location_code:
+                            location[ista] = sta_inv[0][0][0].location_code
+                    else:
+                        sta_inv = inv1.select(network=net[ista],station=sta[ista],location=location[ista]) 
+                        if not sta_inv:
+                            continue 
+
+                    # add the inventory for all components + all time of this tation         
+                    try:ds.add_stationxml(sta_inv) 
+                    except Exception: pass   
+
+                    try:
+                        # get data
+                        t0=time.time()
+                        tr = client.get_waveforms(network=net[ista],station=sta[ista],\
+                            channel=chan[ista],location=location[ista],starttime=s1,endtime=s2)
+                        t1=time.time()
+                    except Exception as e:
+                        print(e,'for',sta[ista]);continue
+                        
+                    # preprocess to clean data  
+                    tr = noise_module.preprocess_raw(tr,sta_inv,prepro_para,date_info)
+                    t2 = time.time()
+
+                    if len(tr):
+                        if location[ista] == '*':
+                            location[ista] = str('00')
+                        new_tags = '{0:s}_{1:s}'.format(chan[ista].lower(),location[ista].lower())
+                        ds.add_waveforms(tr,tag=new_tags)
+
+                    if flag:
+                        print(ds,new_tags);print('downloading data %6.2f s; pre-process %6.2f s' % ((t1-t0),(t2-t1)))
+
+tt1=time.time()
+print('downloading step takes %6.2f s' %(tt1-tt0))
+comm.barrier()
+if rank == 0:
+    sys.exit()
---
+++ src/S2_stacking.py
@@ -0,0 +1,196 @@
+import sys
+import time
+import obspy
+import pyasdf
+import os, glob
+import datetime
+import numpy as np
+import noise_module
+from mpi4py import MPI
+
+if not sys.warnoptions:
+    import warnings
+    warnings.simplefilter("ignore")
+
+'''
+Stacking script of NoisePy:
+    1) read the saved cross-correlation data to do sub-stacks (if needed) and all-time average;
+    2) two ptions to do stacking: linear and pws
+    3) save the outputs in ASDF or SAC format based on user's choice.
+
+Authors: Chengxin Jiang (chengxin_jiang@fas.harvard.edu)
+         Marine Denolle (mdenolle@fas.harvard.edu)
+'''
+
+tt0=time.time()
+
+########################################
+#########PARAMETER SECTION##############
+########################################
+
+# absolute path parameters
+rootpath  = '/Users/chengxin/Documents/Research/Harvard/Kanto'                       # root path for this data processing
+CCFDIR    = os.path.join(rootpath,'CCF')                    # dir where CC data is stored
+STACKDIR  = os.path.join(rootpath,'STACK') 
+
+# assemble path information used to read CC data (stored in ASDF files)
+pfiles    = glob.glob(os.path.join(CCFDIR,'paths_*.lst'))
+
+# load fc_para from S1
+fc_metadata = os.path.join(rootpath,'fft_cc_data.txt')
+fc_para     = eval(open(fc_metadata).read())
+samp_freq   = fc_para['samp_freq']
+start_date  = fc_para['start_date']
+end_date    = fc_para['end_date']
+inc_hours   = fc_para['inc_hours']
+cc_len      = fc_para['cc_len']
+step        = fc_para['step']
+maxlag      = fc_para['maxlag']
+substack    = fc_para['substack']
+substack_len= fc_para['substack_len']
+
+# stacking para
+f_substack = True                                           # whether to do sub-stacking (different from that in S1)
+f_substack_len = 10*cc_len                                  # length for sub-stacking to output
+out_format   = 'ASDF'                                       # ASDF or SAC format for output
+flag         = True                                         # output intermediate args for debugging
+stack_method = 'pws'                                        # linear, pws
+
+# maximum memory allowed per core in GB
+MAX_MEM = 4.0
+
+# make a dictionary to store all variables: also for later cc
+stack_para={'samp_freq':samp_freq,'cc_len':cc_len,'step':step,'rootpath':rootpath,'STACKDIR':\
+    STACKDIR,'start_date':start_date[0],'end_date':end_date[0],'inc_hours':inc_hours,\
+    'substack':substack,'substack_len':substack_len,'maxlag':maxlag,'MAX_MEM':MAX_MEM,\
+    'f_substack':f_substack,'f_substack_len':f_substack_len,'stack_method':stack_method}
+# save fft metadata for future reference
+stack_metadata  = os.path.join(rootpath,'stack_data.txt') 
+
+#######################################
+###########PROCESSING SECTION##########
+#######################################
+
+#--------MPI---------
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+if rank == 0:
+    if not os.path.isdir(STACKDIR):os.mkdir(STACKDIR)
+    # save metadata 
+    fout = open(stack_metadata,'w')
+    fout.write(str(stack_para));fout.close()
+
+    # cross-correlation files
+    ccfiles   = sorted(glob.glob(os.path.join(CCFDIR,'*.h5')))
+    # all station-pair info saved in ASDF
+    if not len(pfiles):
+        raise ValueError('abort! no paths file found in %s'%CCFDIR)
+    paths_all = noise_module.load_pfiles(pfiles)
+    splits  = len(paths_all)
+    if len(ccfiles)==0 or splits==0:
+        raise IOError('Abort! no available CCF data for stacking')
+
+    # make directories for storing stacked data
+    for ii in range(splits):
+        tr   = paths_all[ii].split('s')
+        tdir = os.path.join(STACKDIR,tr[0]+'.'+tr[1]+'.'+tr[3])
+        if not os.path.isdir(tdir):os.mkdir(tdir)
+else:
+    splits,ccfiles,paths_all = [None for _ in range(3)]
+
+# broadcast the variables
+splits    = comm.bcast(splits,root=0)
+ccfiles   = comm.bcast(ccfiles,root=0)
+paths_all = comm.bcast(paths_all,root=0)
+extra = splits % size
+
+# MPI loop: loop through each user-defined time chunck
+for ipath in range (rank,splits+size-extra,size):
+    if ipath<splits:
+        t0=time.time()
+
+        if flag:print('rank %d for station-pair %s'%(ipath,paths_all[ipath]))
+        # source folder
+        ttr   = paths_all[ipath].split('s')
+        idir  = ttr[0]+'.'+ttr[1]+'.'+ttr[3]
+        outfn = ttr[0]+'.'+ttr[1]+'.'+ttr[3]+'_'+ttr[4]+'.'+ttr[5]+'.'+ttr[7]+'.h5'
+        if flag:print('source %s and output %s'%(idir,outfn))
+
+        # crude estimation on memory needs (assume float32)
+        num_chunck  = len(ccfiles)
+        num_segmts  = int(np.round(inc_hours*3600/substack_len))
+        npts_segmt  = int(2*maxlag*samp_freq)+1
+        memory_size = num_chunck*num_segmts*npts_segmt*4/1024**3
+        if memory_size > MAX_MEM:
+            raise ValueError('Require %s G memory (%s GB provided)! Cannot load cc data all once!' % (memory_size,MAX_MEM))
+        if flag:
+            print('Require %6.4fG memory (%s G provided)!' % (memory_size,MAX_MEM))
+            
+        # open array to store fft data/info in memory
+        cc_array = np.zeros((num_chunck*num_segmts,npts_segmt),dtype=np.float32)
+        cc_time  = np.zeros(num_chunck*num_segmts,dtype=np.float)
+
+        # loop through all time-chuncks
+        iseg = 0
+        station_pair = paths_all[ipath]
+        for ifile in range(len(ccfiles)):
+            ds=pyasdf.ASDFDataSet(ccfiles[ifile],mode='r')
+            if not ds.auxiliary_data.list(): continue
+            path_list = ds.auxiliary_data['CCF'].list()            
+            if station_pair not in path_list:
+                if flag:print('continue! no data in %s'%ccfiles[ifile]);continue
+    
+            # load the data by segments
+            tdata = ds.auxiliary_data['CCF'][station_pair].data[:]
+            ttime = ds.auxiliary_data['CCF'][station_pair].parameters['time']
+            if ifile==0:
+                tparameters = ds.auxiliary_data['CCF'][station_pair].parameters
+            for ii in range(tdata.shape[0]):
+                cc_array[iseg] = tdata[ii]
+                cc_time[iseg]  = ttime[ii]
+                iseg+=1
+        t1=time.time()
+        if flag:print('loading CCF data takes %6.2fs'%(t1-t0))
+
+        # do substacking if needed
+        if f_substack:
+            substacks,stime,num_stacks = noise_module.do_stacking(cc_array[:iseg],cc_time[:iseg],f_substack_len,stack_para)
+            t2=time.time()
+            if flag:print('finished substacking, which takes %6.2fs'%(t2-t1))
+            
+            if out_format=='ASDF':
+                stack_h5 = os.path.join(STACKDIR,idir+'/'+outfn)
+                with pyasdf.ASDFDataSet(stack_h5,mpi=False) as ds:
+                    for iii in range(substacks.shape[0]):
+                        tparameters['time']  = stime
+                        tparameters['ngood'] = num_stacks
+                        tpath     = ttr[2][-1]+ttr[6][-1]
+                        data_type = 'T'+str(int(stime[iii]))
+                        ds.add_auxiliary_data(data=substacks[iii], data_type=data_type, path=tpath, parameters=tparameters)
+        
+        # do all stacking
+        t3=time.time()
+        allstacks,alltime,num_stacks = noise_module.do_stacking(cc_array,cc_time,0,stack_para)
+        t4=time.time()
+        if out_format=='ASDF':
+            stack_h5 = os.path.join(STACKDIR,idir+'/'+outfn)
+            with pyasdf.ASDFDataSet(stack_h5,mpi=False) as ds:
+                tparameters['time']  = alltime
+                tparameters['ngood'] = num_stacks
+                tpath     = ttr[2][-1]+ttr[6][-1]
+                data_type = 'Allstack'
+                ds.add_auxiliary_data(data=allstacks, data_type=data_type, path=tpath, parameters=tparameters)
+
+        t5 = time.time()
+        if flag:print('takes %6.2fs to process one chunck data, %6.2fs for all stacking' %(t5-t0,t4-t3))
+        
+tt1 = time.time()
+print('it takes %6.2fs to process step 2 in total' % (tt1-tt0))
+comm.barrier()
+
+# merge all path_array and output
+if rank == 0:
+    sys.exit()
+            
